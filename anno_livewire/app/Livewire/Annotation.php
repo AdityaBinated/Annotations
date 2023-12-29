0233d20171421847db1c067aa9d591344d58f06f
AdityaBinated <adityap@binated.in>
2023-12-29 17:32:30 +0530

    commit

--- anno_livewire/app/Livewire/Annotation.php
+++ anno_livewire/app/Livewire/Annotation.php
@@ -4,8 +4,26 @@ namespace App\Livewire;
 
 use Livewire\Component;
 
+
 class Annotation extends Component
 {
+   
+   public $annotations;
+   
+
+   public function saveAnnotations($annotations)
+   {
+    //    dd(4);
+   
+      
+    
+   
+
+    }  
+
+
+
+
     public function render()
     {
         return view('livewire.annotation');
--- anno_livewire/resources/views/livewire/annotation.blade.php
+++ anno_livewire/resources/views/livewire/annotation.blade.php
@@ -1,3 +1,35 @@
-<div>
-    
+
+
+ 
+
+
+  <div style="width: 100%; display: flex;" x-init="
+    (function() {
+      var anno = Annotorious.init({
+        image: 'hallstatt'
+       
+      });
+      
+      
+      anno.on('createAnnotation', function(annotation, overrideId) {
+          console.log('created');
+          $wire.$call('saveAnnotations',anno.getAnnotations());
+        });
+      anno.on('deleteAnnotation', function(annotation, overrideId) {
+        console.log('deleted');
+        $wire.$call('saveAnnotations',anno.getAnnotations());
+        });
+        anno.on('updateAnnotation', function(annotation, overrideId) {
+        console.log('updated');
+        $wire.$call('saveAnnotations',anno.getAnnotations());
+        });
+    })()"> 
+
+
+   <div>
+    <img id= "hallstatt" src="https://upload.wikimedia.org/wikipedia/commons/e/e4/Hallstatt_300.jpg">
+  </div>
+
 </div>
+
+
